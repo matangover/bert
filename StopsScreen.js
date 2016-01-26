c53719c770348e024b881e4e0b0b25ad46c4b973
Matan Gover <matangover@gmail.com>
2016-01-26 03:55:14 +0200

    Add fetching of location

--- StopsScreen.js
+++ StopsScreen.js
@@ -11,15 +11,25 @@ import React, {
 import stopsData from './assets/stops.json'
 
 class StopsScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {position: null};
+  }
+
   render() {
-    var stops = this.getStops();
-    var children = stops.map(stop => (
+    if (!this.state.position) {
+      var children = <Text>Loading...</Text>;
+    } else {
+      var stops = this.getStops();
+      var children = stops.map(stop => (
         <TouchableHighlight
-            key={stop.stop_code}
-            onPress={() => this.props.navigator.push({name: 'bus_times', stop_id: stop.stop_code})}>
-                <Text>Stop {stop.stop_code}</Text>
+          key={stop.stop_code}
+          onPress={() => this.props.navigator.push({name: 'bus_times', stop_id: stop.stop_code})}>
+            <Text>Stop {stop.stop_code}</Text>
         </TouchableHighlight>
-    ));
+      ));
+    }
+
     return (
       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
         <Text>Stops:</Text>
@@ -28,8 +38,18 @@ class StopsScreen extends Component {
     );
   }
 
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({position});
+      },
+      (error) => alert(error.message),
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+    );
+  }
+
   getStops() {
-      return stopsData.splice(0, 10);
+    return stopsData.splice(0, 10);
   }
 }
 
